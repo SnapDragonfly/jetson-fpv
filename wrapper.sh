76cd1df8819bcf96ff3ccb8265247b301b0c86fd
Daniel Li <lida_mail@163.com>
2024-11-20 11:00:05 +0800

    add python environment printf in help

--- wrapper.sh
+++ wrapper.sh
@@ -28,6 +28,11 @@ help() {
     for module in "${MODULES[@]}"; do
         echo "  $module"
     done
+    echo
+    echo "System Environment:"
+    python3 --version
+    python3 -c "import cv2; print(cv2.getBuildInformation())" | grep -E "CUDA|GStreamer"
+    python3 -c "import cv2; print('OpenCV version:', cv2.__version__, ', CUDA support:', cv2.cuda.getCudaEnabledDeviceCount() > 0)"
 }
 
 # Check if the module name is valid
