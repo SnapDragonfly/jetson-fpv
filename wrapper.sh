343d14729bee38e2729b3a9cc282350b944b1215
Daniel Li <lida_mail@163.com>
2024-11-20 14:18:00 +0800

    add yolo version help

--- wrapper.sh
+++ wrapper.sh
@@ -46,13 +46,17 @@ help() {
     eval "python3 -c \"import cv2; print(cv2.getBuildInformation())\" $CMD_NULL" | grep -E "CUDA|GStreamer"
 
     # OpenCV version and CUDA support
-    eval "python3 -c \"import cv2; print('OpenCV version:', cv2.__version__, ', CUDA support:', cv2.cuda.getCudaEnabledDeviceCount() > 0)\" $CMD_NULL" | grep -v "EGL display"
+    eval "python3 -c \"import cv2; print('     OpenCV version:', cv2.__version__, ' CUDA', cv2.cuda.getCudaEnabledDeviceCount() > 0)\" $CMD_NULL" | grep -v "EGL"
+
+    # YOLO version
+    YOLO_VERSION=$(eval yolo version $CMD_NULL | grep -v "EGL")
+    echo "       YOLO version: $YOLO_VERSION"
 
     # PyTorch version
-    eval "python3 -c \"import torch; print('Torch version:', torch.__version__)\" $CMD_NULL" | grep -v "EGL display"
+    eval "python3 -c \"import torch; print('      Torch version:', torch.__version__)\" $CMD_NULL" | grep -v "EGL"
 
     # Torchvision version
-    eval "python3 -c \"import torchvision; print('Torchvision version:', torchvision.__version__)\" $CMD_NULL" | grep -v "EGL display"
+    eval "python3 -c \"import torchvision; print('Torchvision version:', torchvision.__version__)\" $CMD_NULL" | grep -v "EGL"
 }
 
 # Check if the module name is valid
