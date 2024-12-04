e56a7bb75134c6897e1ba4f007d160610cc12a72
Daniel Li <lida_mail@163.com>
2024-12-04 16:36:14 +0800

    DeepStream: remove gst buffer which didn't affect the FPS rate

--- utils/deepstream.py
+++ utils/deepstream.py
@@ -44,13 +44,13 @@ class VideoStreamer:
         """
         if self.input_codec == "h264":
             pipeline_str = (
-                f"udpsrc port={self.port} buffer-size=8388608 ! "
+                f"udpsrc port={self.port} ! "
                 "application/x-rtp,encoding-name=H264,payload=96 ! "
                 "rtph264depay ! h264parse ! nvv4l2decoder ! nv3dsink name=sink sync=0"
             )
         else:
             pipeline_str = (
-                f"udpsrc port={self.port} buffer-size=8388608 ! "
+                f"udpsrc port={self.port} ! "
                 "application/x-rtp,encoding-name=H265,payload=96 ! "
                 "rtph265depay ! h265parse ! nvv4l2decoder ! nv3dsink name=sink sync=0"
             )
