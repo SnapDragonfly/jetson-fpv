a4e61d9d8d7e5f3bc55a7d743901d2cec3388d04
Daniel Li <lida_mail@163.com>
2025-01-28 10:11:50 +0800

    yolo: remove auto-generated engine file

--- install/install_yolo.sh
+++ install/install_yolo.sh
@@ -3,3 +3,11 @@
 sudo apt-get install -y aptitude tree mpv
 sudo pip3 install ultralytics
 
+cd ../model/
+yolo export model=yolo11n.pt format=onnx
+yolo export model=yolov5nu.pt format=onnx
+yolo export model=yolov8n.pt format=onnx
+yolo export model=yolo11n.pt format=engine int8=True
+yolo export model=yolov5nu.pt format=engine int8=True
+yolo export model=yolov8n.pt format=engine int8=True
+cd -
