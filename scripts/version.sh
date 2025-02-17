870402209573b23cdf764145f291ff513dd0bdf1
Daniel Li <lida_mail@163.com>
2025-02-17 16:20:05 +0800

    version: add new version controls

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,16 +4,31 @@ cd scripts
 source common/versions.sh
 cd ..
 
+CMD_NULL=" 2>/dev/null"
+export DISPLAY=:0
+
 echo
 jetson_release
 
+echo
+echo "--------------------------------"
+echo "NVIDIA SDK:"
 # Deepstream C/C++ SDK version
 DEEPSTREAM_VERSION=$(get_deepstream_version)
 echo "DeepStream C/C++ SDK version: $DEEPSTREAM_VERSION"
-echo
 
-CMD_NULL=" 2>/dev/null"
-export DISPLAY=:0
+# Get version for jetson-inference (short commit hash)
+JETSON_INFERENCE_VERSION=$(git submodule status | grep 'module/jetson-inference' | awk '{print $1}' | cut -c1-7)
+JETSON_INFERENCE_DIRTY=$(cd module/jetson-inference && git status --porcelain | grep -q . && echo " (dirty)" || echo "")
+echo "    jetson-inference version: $JETSON_INFERENCE_VERSION$JETSON_INFERENCE_DIRTY"
+
+# Get version for jetson-utils (short commit hash)
+JETSON_UTILS_VERSION=$(git submodule status | grep 'module/jetson-utils' | awk '{print $1}' | cut -c1-7)
+JETSON_UTILS_DIRTY=$(cd module/jetson-utils && git status --porcelain | grep -q . && echo " (dirty)" || echo "")
+echo "        jetson-utils version: $JETSON_UTILS_VERSION$JETSON_UTILS_DIRTY"
+
+echo
+echo "--------------------------------"
 echo "Python Environment:"
 
 # Python version
@@ -46,7 +61,12 @@ eval "python3 -c \"import pyds; print(' DeepStream SDK version:', pyds.__version
 eval "pip list $CMD_NULL" | grep onnxruntime | awk '{printf "%-15s version: %s\n", $1, $2}'
 
 echo
+echo "--------------------------------"
 echo "FPV Environment:"
 
-echo "jetson-fpv Version: $(git rev-parse --short HEAD) $(git diff --quiet && echo '' || echo 'dirty')"
+echo "jetson-fpv version: $(git rev-parse --short HEAD) $(git diff --quiet && echo '' || echo 'dirty')"
+
+WFB_VERSION=$(wfb-cli --version | grep -oP 'WFB-ng version \K[\d\.]+')
+echo "    WFB-ng version: $WFB_VERSION"
+
 ./utils/msposd/msposd --help | grep "Version" | sed 's/Version: \(.*\), compiled at: \(.*\)/    MSPOSD version: \1 \2/'
