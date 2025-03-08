4c4b4976631b2f64d575719d2e4d3e14e48c5876
Daniel Li <lida_mail@163.com>
2025-03-08 13:41:38 +0800

    yolo: use yolo default precision

--- install/install_yolo.sh
+++ install/install_yolo.sh
@@ -7,7 +7,7 @@ cd ../model/
 yolo export model=yolo11n.pt format=onnx
 yolo export model=yolov5nu.pt format=onnx
 yolo export model=yolov8n.pt format=onnx
-yolo export model=yolo11n.pt format=engine int8=True
-yolo export model=yolov5nu.pt format=engine int8=True
-yolo export model=yolov8n.pt format=engine int8=True
+yolo export model=yolo11n.pt format=engine
+yolo export model=yolov5nu.pt format=engine
+yolo export model=yolov8n.pt format=engine
 cd -
